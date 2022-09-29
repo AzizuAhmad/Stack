56752844c6bd24e56c8413218c5374f1c13f4276
AzizuAhmad <92623900+AzizuAhmad@users.noreply.github.com>
2022-09-29 15:25:08 +0700

    Add files via upload

---
+++ stack2.cpp
@@ -0,0 +1,171 @@
+#include <iostream>
+#include <conio.h>
+#include <windows.h>
+#define maksimal 5
+
+void gotoxy(int x, int y)
+{
+    COORD coord;
+    coord.X = x;
+    coord.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+
+using namespace std;
+
+int top = 1;
+struct paket
+{
+
+    char jenis_brg[30];
+    int berat;
+    char kota[30];
+};
+
+struct paket kirim[maksimal];
+
+/*******************************************/
+bool isFull()
+{
+    if (top == maksimal)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+bool isEmpty()
+{
+    if (top == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+void bersihlayar()
+{
+    for (int a = 0; a < 80; a++)
+    {
+        for (int b = 0; b <= 24; b++)
+        {
+            gotoxy(a, b);
+            cout << " ";
+        }
+    }
+}
+
+void tambahdata()
+{
+    if (isFull())
+    {
+        cout << "data penuh" << endl;
+    }
+    else
+    {
+        gotoxy(20, 15);
+        cout << "Inputkan Jenis Barang    :";
+        cin >> kirim[top].jenis_brg;
+        gotoxy(20, 16);
+        cout << "Inputkan Berat           :";
+        cin >> kirim[top].berat;
+        gotoxy(20, 17);
+        cout << "Inputkan Kota Tujuan     :";
+        cin >> kirim[top].kota;
+        top++;
+    }
+}
+
+void tampildata()
+{
+    if (isEmpty())
+    {
+        cout << "data kosong" << endl;
+    }
+    else
+    {
+        gotoxy(40, 10);
+        cout << "Daftar Kiriman Paket" << endl;
+        for (int a = 1; a < top; a++)
+        {
+            gotoxy(40, 19 - a);
+            cout << a << " ";
+            cout << kirim[a].jenis_brg << " ";
+            cout << kirim[a].berat << " ";
+            cout << kirim[a].kota << endl;
+        }
+        gotoxy(40, 20);
+        cout << "Press Any Key...";
+        getch();
+        /*harus Ijin --> #include<conio.h>*/
+    }
+}
+
+void hapusdata()
+{
+    if (isEmpty())
+    {
+        cout << "Stack Masih Kosong" << endl;
+        return;
+    }
+    else
+    {
+        gotoxy(40, 12);
+        cout << "Data Terakhir yang anda Hapus Adalah :" << endl;
+        gotoxy(40, 13);
+        cout << kirim[top - 1].jenis_brg << " ";
+        cout << kirim[top - 1].berat << " Kg ";
+        cout << kirim[top - 1].kota << endl
+             << endl;
+        top--;
+    }
+    gotoxy(40, 15);
+    cout << "Press Any Key...";
+    getch();
+}
+
+int main()
+{
+    int pilih;
+
+    do
+    {
+        bersihlayar();
+        gotoxy(1, 1);
+        cout << "MENU UTAMA";
+        gotoxy(1, 2);
+        cout << "1. Tambah Data";
+        gotoxy(1, 3);
+        cout << "2. Hapus Data Terakhir" << endl;
+        gotoxy(1, 4);
+        cout << "3. Tampil Data" << endl;
+        gotoxy(1, 5);
+        cout << "9. Keluar" << endl;
+
+        gotoxy(1, 7);
+        cout << "Pilihan Anda [1-9] : ";
+        cin >> pilih;
+        if (pilih == 1)
+        { /*Menambah data baru diletakan di DATAKE*/
+            tambahdata();
+        }
+        if (pilih == 2)
+        { /*Menghapus Data Terakhir (top-1*/
+            hapusdata();
+        }
+        if (pilih == 3)
+        {
+            /*Menampilkan Data mulai
+            data ke-1 hingga top*/
+            tampildata();
+        }
+
+    } while (pilih != 9);
+    return 0;
+}
